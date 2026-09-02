c290fc2e9bb669d344a517c6d7ce17123a46b2df
Matteo Lovato <matteolovatoai@proton.me>
2026-09-02 23:10:42 +0200

    Aggiunta chiamata a gemini

---
+++ backend/rag_engine.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+
+client = genai.Client()
+
+
+def generate_ai_response(prompt: str) -> str:
+    response = client.models.generate_content(
+        model="gemini-3.5-flash-lite", contents=prompt
+    )
+    return response.text or "Errore: Il modello non ha generato una risposta."
