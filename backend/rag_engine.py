fe67a4dc0a9880a5aaffe0f79d34a7ee2dbcdee6
Matteo Lovato <matteolovatoai@proton.me>
2026-09-03 14:39:34 +0200

    creato client e logica di creazione collezione

--- backend/rag_engine.py
+++ backend/rag_engine.py
@@ -1,9 +1,14 @@
+import chromadb
 from dotenv import load_dotenv
 from google import genai
 
 load_dotenv()
 
 client = genai.Client()
+
+chroma_client = chromadb.PersistentClient(path="./chroma_db")
+
+documents = chroma_client.get_or_create_collection(name="documenti")
 
 
 def generate_ai_response(prompt: str) -> str:
